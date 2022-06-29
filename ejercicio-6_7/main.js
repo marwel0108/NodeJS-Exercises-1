e065fc6fa48d6e613d4ed2f8a58e9b87d47fa994
Emmanuel Arellano <marwel0108@gmail.com>
2022-06-29 00:35:24 -0500

    FIX: main.js add.

---
+++ ejercicio-6_7/main.js
@@ -0,0 +1,15 @@
+const Notas = require('./classes/Notas')
+
+const n = new Notas();
+
+n.agregarNota('Lord of the Rings', 'J. R. R. Tolkien');
+n.agregarNota('IT', 'Stephen King');
+n.agregarNota('Harry Potter and the Globbet of Fire', 'J. K. Rowling');
+n.agregarNota('Dune', 'Frank Herbert');
+n.agregarNota('Coraline', 'Neil Gaiman');
+
+n.obtenerNotas()
+n.quitarNota(2);
+n.obtenerNotas();
+
+n.leerNota(3);
